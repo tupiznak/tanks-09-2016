a5f86636a767079f3716df6e2961cfde468f8657
tupiznak <akej-vonavi@mail.ru>
2016-11-03 21:02:49 +0300

    to postgresql

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,5 @@
 create table users (
-  id int serial primary key,
+  id serial primary key,
   login varchar(45) DEFAULT NULL UNIQUE,
   email VARCHAR(45) DEFAULT NULL,
   password VARCHAR(45) DEFAULT NULL
